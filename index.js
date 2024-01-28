3a1c945955a14567632300016b1e2ed4ba1aea47
Rifat <1905094@ugrad.cse.buet.ac.bd>
2024-01-28 16:54:15 +0600

    router name fixed

--- index.js
+++ index.js
@@ -16,7 +16,9 @@ const trailRouter = require("./router/trial");
 const conferenceRouter = require("./router/conference");
 const userRouter = require("./router/user");
 const paperRouter = require("./router/paper");
-const assignRouter = require("./router/assign");const authRouter = require("./router/auth");
+const assignRouter = require("./router/assign");
+const authRouter = require("./router/auth");
+const requestRouter = require("./router/request");
 
 
 app.use("/trial", trailRouter);
@@ -25,6 +27,7 @@ app.use("/user", userRouter);
 app.use("/paper",paperRouter);
 app.use("/assign", assignRouter);
 app.use("/auth", authRouter);
+app.use("/request", requestRouter);
 
 
 
