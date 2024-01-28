00326f42c9bb3b6f3b24d50fe5a139fbc23b2445
Avi Dewan <1905120@ugrad.cse.buet.ac.bd>
2024-01-28 18:22:46 +0600

    reviewer route added

--- router/reviewer.js
+++ router/reviewer.js
@@ -36,4 +36,39 @@ router.put("/review", async (req, res) => {
     }
 });
 
+
+// get conference paper details ( with authors)
+// we have a paper table ( primary key = paper_id, foreign id = conference_id)
+// we have // paperReviewer table: primary_key(user_id, paper_id), rating and review
+
+
+router.get("/assignedPapers/:user_id", async (req, res) => {
+    try {
+      
+      const user_id = req.params.user_id;
+
+      const { data } = await db.from('assignedReviewer').select('paper_id, rating, review, paper( paper_title, abstract, pdf_link, related_fields)').eq('user_id', user_id);
+
+      // change the data array so that every item is directly of the json.. not data[0].papr.paper_title . rather data[0].paper_title
+
+      // Restructure the data array
+      const restructuredData = data.map(item => ({
+            paper_id: item.paper_id,
+            rating: item.rating,
+            review: item.review,
+            review_status: item.rating !== null && item.review !== null ? "reviewed" : "not reviewed",
+            ...item.paper
+       }));
+
+       res.json(restructuredData);
+
+
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send('Internal Server Error');
+    }
+});
+
+
+
 module.exports = router;
