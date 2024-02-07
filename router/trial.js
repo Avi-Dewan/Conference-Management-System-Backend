a512e90e95d80325508a73947ab7f6b2fd6228aa
Avi Dewan <1905120@ugrad.cse.buet.ac.bd>
2024-02-07 22:57:13 +0600

    email train added

--- router/trial.js
+++ router/trial.js
@@ -1,9 +1,12 @@
 // trial.js
 const express = require('express');
 const db = require('../db/database'); 
+const nodemailer = require("nodemailer");
+const dotenv = require("dotenv");
 
 const router = express.Router();
 
+dotenv.config();
 
 // Get all trials
 router.get('/', async (req, res) => {
@@ -107,5 +110,46 @@ router.delete('/:id', async (req, res) => {
     res.status(500).send('Internal Server Error');
   }
 });
+
+
+
+router.post('/sendMail', async (req, res) => {
+
+  const {email, message} = req.body;
+
+    // Send the PIN to the user's email
+    const transporter = nodemailer.createTransport({
+      service: "Gmail",
+      auth: {
+        user: `${process.env.DB_GMAIL}`,
+        pass: `${process.env.DB_GMAIL_PASSWORD}`,
+      },
+    });
+
+
+    const mailOptions = {
+      from: process.env.DB_GMAIL,
+      to: email,
+      subject: "Test Email from Conference Management System",
+      html: `
+      <h1>Welcome to Conference Management System</h1>
+      <p>${message}</p>
+      <a href="https://www.ggogle.com" style="background-color: 
+      blue; color: white; padding: 10px 20px; text-decoration: none;">Click Me</a>
+  `,
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.log(error);
+        return res.status(500).json({ error: "Email could not be sent" });
+      } else {
+        console.log("Email sent: " + info.response);
+        return res.status(200).json({ message: "Email send successfully" });
+      }
+    });
+
+});
+
 
 module.exports = router;
